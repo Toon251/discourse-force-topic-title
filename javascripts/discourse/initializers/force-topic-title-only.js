81359844585829fee1d736886326159ff5d735c8
Toon251 <toon251@gmail.com>
2025-05-28 09:25:15 +0000

    Change api to modifyClass

--- javascripts/discourse/initializers/force-topic-title-only.js
+++ javascripts/discourse/initializers/force-topic-title-only.js
@@ -2,12 +2,28 @@ import { apiInitializer } from 'discourse/lib/api';
 
 
 export default apiInitializer('1.14.0', (api) => {
-    
-    api.routeTitle((currentRoute) => {
-      console.log(currentRoute);
-    if (currentRoute.name === "topic") {
-      return currentRoute.model.title;
+    api.modifyClass("service:head-data", {
+    pluginId: "custom-topic-title",
+
+    customTopicTitle() {
+      const currentRoute = this.router.currentRoute;
+      
+      // ตรวจสอบว่าอยู่ใน route topic และมี model กำหนด
+      if (currentRoute && currentRoute.name === "topic" && currentRoute.model) {
+        return currentRoute.model.title;
+      }
+      return null;
+    },
+
+    // override headTitle หรือ title getter
+    get title() {
+      console.log("get title");
+      const customTitle = this.customTopicTitle();
+      if (customTitle) {
+        return customTitle;
+      }
+      // fallback ให้เรียก getter ต้นฉบับ
+      return this._super(...arguments);
     }
-    return null;
   });
 });
